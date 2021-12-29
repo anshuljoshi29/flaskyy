52257adaa0eae9bcb753a993dfaed607426fdfde
Anshul <ajanshul2001@gmail.com>
2021-12-29 19:05:39 +0530

    Enter

---
+++ website/auth.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    return render_template("login.html", boolean=True)
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout<p>"
+
+@auth.route('/sign-up',methods=['GET','POST'])
+def sign_up():
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName=request.form.get('firstName')
+        password1=request.form.get('password1')
+        password2=request.form.get('password2')
+
+        if len(email) < 4:
+            flash('email should be greater than 4 char',category='error')
+        elif len(firstName) < 2:
+            flash('first name should be greater than 4 char',category='error')
+        elif password1 != password2 :
+            flash('password don\'t match',category='error')
+        elif len(password1) < 7:
+            flash('password muct be atleast 7 char',category='error')
+        else:
+            #add useer to database
+            flash('Account  created',category='success')
+
+
+    return render_template("sign_up.html")
